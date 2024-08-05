017171dc625527e716b22f4fff2a141d97dc6c62
Mehmet Emre Akca <96843001+memreakca@users.noreply.github.com>
2024-08-05 17:17:54 +0300

    a

--- Rocketman/Assets/Player.cs
+++ Rocketman/Assets/Player.cs
@@ -267,6 +267,7 @@ public class Player: MonoBehaviour
             if (distanceToPlatformCenter <= closeDistanceThreshold)
             {
                 SpawnLandingPrefab();
+
                 HighScore++;
             }
         }
